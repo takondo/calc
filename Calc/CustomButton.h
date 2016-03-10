640f8e2a9daa293d13490e6b15b688196168ee4c
0A3009 <tkmknd@gmail.com>
2016-03-10 14:58:05 +0900

    edit

---
+++ Calc/CustomButton.h
@@ -0,0 +1,13 @@
+//
+//  CustomButton.h
+//  Calc
+//
+//  Created by 0A3009 on 2016/03/07.
+//  Copyright © 2016年 0A3009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomButton : UIButton
+
+@end
---
+++ Calc/CustomButton.m
@@ -0,0 +1,21 @@
+//
+//  CustomButton.m
+//  Calc
+//
+//  Created by 0A3009 on 2016/03/07.
+//  Copyright © 2016年 0A3009. All rights reserved.
+//
+
+#import "CustomButton.h"
+
+@implementation CustomButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
